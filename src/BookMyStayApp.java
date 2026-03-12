389b1ac7778909a8f58e74cc142e1159882db690
shubhang-8 <shubhangkediajaipiur@gmail.com>
2026-03-12 09:43:22 +0530

    updated feature

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class BookMyStayApp {
+
+
+    public static void main(String[] args) {
+
+        // Display welcome message
+        System.out.println("=================================");
+        System.out.println(" Welcome to Book My Stay System ");
+        System.out.println(" Version: 1.0 ");
+        System.out.println("=================================");
+        System.out.println("Your trusted hotel booking platform!");
+
+    }
 }
